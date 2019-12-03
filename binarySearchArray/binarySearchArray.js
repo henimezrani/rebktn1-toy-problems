0364f884e043697efaaaf37b5b7e278f579c4041
henimezrani <henimezrani@gmail.com>
2019-12-03 09:07:11 +0100

    Binary search solved

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,6 +10,18 @@
  * console.log(index); // null
  */
 
-var binarySearch = function (array, target) {
+var binarySearch = function (array, target, start, end) {
+	var start = start || 0;
+	var end = end || array.length-1;
+	var mid = Math.floor(end-start+1/2);
+	if (array.length === 0){
+		return null;
+	} else if (array[mid] === target) {
+		return mid;
+	} else if (array[mid] > target) {
+		return binarySearch(array, target, start, mid-1)
+	} else {
+		return binarySearch(array, target, mid+1, end)
+	}
 };
 
