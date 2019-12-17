3579fad0dc92be0f7a09c6394132a5232a8c840b
henimezrani <henimezrani@gmail.com>
2019-12-17 09:00:08 +0100

    pipe compose solved

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -1,7 +1,7 @@
 /*
  * Write Compose and Pipe functions.
- * 
- * Step 1: Implement the function Compose: 
+ *
+ * Step 1: Implement the function Compose:
  *
  * Compose should return a function that is the composition of a list of
  * functions of arbitrary length.
@@ -19,11 +19,11 @@
  * Step 2: Implement the function Pipe:
  *
  * Pipe composes a series of functions and returns the resulting function.
- * 
+ *
  * Each function is called on the return value of the preceding function.
  *
  * You can view pipe as moving left to right through its arguments.
- * 
+ *
  * Pipe Example:
  *  var add2 = function(number){ return number + 2; }
  *  var multiplyBy3 = function(number){ return number * 3; }
@@ -34,7 +34,31 @@
 'use strict';
 
 var compose = function() {
+  var arr = [];
+  for (var i = 0; i < arguments.length ; i++){
+    arr[i] = arguments[i]
+  }
+  return function(arg) {
+    var result;
+    for (var i = arr.length-1 ; i >=0 ; i--) {
+      arg = result || arg;
+      result = arr[i](arg);
+    }
+    return result
+  }
 };
 
 var pipe = function() {
+  var arr = [];
+  for (var i = 0; i < arguments.length ; i++){
+    arr[i] = arguments[i]
+  }
+  return function(arg) {
+    var result;
+    for (var i = 0 ; i < arr.length ; i++) {
+      arg = result || arg;
+      result = arr[i](arg);
+    }
+    return result
+  }
 };
