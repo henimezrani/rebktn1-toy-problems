d507bd86e3d2bb610e7f81a6091861b412aeed9f
henimezrani <henimezrani@gmail.com>
2019-12-27 09:09:54 +0100

    solved

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -1,6 +1,6 @@
 /**
  * Write a stack using your preferred instantiation pattern. Implement a min function
- * that returns the minimum value of all the elements in the stack in constant time.stack.  
+ * that returns the minimum value of all the elements in the stack in constant time.stack.
 
  * All of the functions in the Stack should run in constant time!
  *
@@ -17,24 +17,40 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
+var Stack = function() {
+  this.storage = []
+  this.end = 0
+  this.minArr = [];
+}
 
-  // add an item to the top of the stack
-    this.push = function(value) {
-    };
+// add an item to the top of the stack
+Stack.prototype.push = function(value) {
+  if (this.storage.length === 0 || value < this.minArr[this.minArr.length-1]) {
+    this.minArr.push(value);
+  }
+  this.storage.push(value);
+  this.end++;
+};
 
-  // remove an item from the top of the stack
-    this.pop = function() {
-    };
+// remove an item from the top of the stack
+Stack.prototype.pop = function() {
+  var tmp = this.storage.pop()
+  if (tmp === this.minArr[this.minArr.length-1]){
+    this.minArr.pop();
+  }
+  this.end--;
+};
 
-  // return the number of items in the stack
-    this.size = function() {
-    };
-  
-  // return the minimum value in the stack
-    this.min = function() {
+// return the number of items in the stack
+Stack.prototype.size = function() {
+  return this.end;
+};
 
-    };
+// return the minimum value in the stack
+Stack.prototype.min = function() {
+  if (this.minArr.length === 0)
+    return undefined;
+  return this.minArr[this.minArr.length-1];
+};
 
-  };
 
