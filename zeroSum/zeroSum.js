efd50744c5c1110ac6c579a907dabb70d1379127
henimezrani <henimezrani@gmail.com>
2019-12-31 09:04:19 +0100

    solved

--- zeroSum/zeroSum.js
+++ zeroSum/zeroSum.js
@@ -7,3 +7,16 @@ Constraints: Solve this linear time complexity O(n)
 zeroSum([ 1, 3, 2, -3 ]) // true
 zeroSum([ 5, 7, 2, 9 ]) // false
 */
+
+
+function zeroSum(array) {
+  var expectedNums = {};
+  for (var i = 0 ; i < array.length ; i++) {
+    var expected = -array[i]
+    if (expectedNums[array[i]]) {
+      return true;
+    }
+    expectedNums[expected] = true
+  }
+  return false
+}
