ffa6018e92a35c4334b8f229ecfb0925f77ff0c7
henimezrani <henimezrani@gmail.com>
2020-01-13 09:17:37 +0100

    solved

--- spiralTraversal/spiralTraversal.js
+++ spiralTraversal/spiralTraversal.js
@@ -4,37 +4,37 @@ Spiral Traversal
 Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
 and prints out every value found, but in a spiral from the upper left in to the center.
 
-spiralTraversal([ 
-  [ 1, 2, 3, 4, 5 ], 
-  [ 6, 7, 8, 9, 10 ], 
-  [ 11, 12, 13, 14, 15 ], 
-  [ 16, 17, 18, 19, 20 ], 
-  [ 21, 22, 23, 24, 25 ] 
-]) 
+spiralTraversal([
+  [ 1, 2, 3, 4, 5 ],
+  [ 6, 7, 8, 9, 10 ],
+  [ 11, 12, 13, 14, 15 ],
+  [ 16, 17, 18, 19, 20 ],
+  [ 21, 22, 23, 24, 25 ]
+])
 
 // output: [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
 
 
-spiralTraversal([ 
-  [ 1, 2, 3, 4, 5 ], 
-  [ 6, 7, 8, 9, 10 ], 
-  [ 11, 12, 13, 14, 15 ], 
-  [ 16, 17, 18, 19, 20 ], 
-  [ 21, 22, 23, 24, 25 ] 
-])	
+spiralTraversal([
+  [ 1, 2, 3, 4, 5 ],
+  [ 6, 7, 8, 9, 10 ],
+  [ 11, 12, 13, 14, 15 ],
+  [ 16, 17, 18, 19, 20 ],
+  [ 21, 22, 23, 24, 25 ]
+])
 
 // output: [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
 
-spiralTraversal([ 
-  [ 1, 2, 3 ], 
-  [ 4, 5, 6 ], 
-  [ 7, 8, 9 ], 
-  [ 10, 11, 12 ], 
-  [ 13, 14, 15 ], 
-  [ 16, 17, 18 ], 
-  [ 19, 20, 21 ], 
-  [ 22, 23, 24 ] 
-])	
+spiralTraversal([
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ],
+  [ 10, 11, 12 ],
+  [ 13, 14, 15 ],
+  [ 16, 17, 18 ],
+  [ 19, 20, 21 ],
+  [ 22, 23, 24 ]
+])
 
 // output: [ 1, 2, 3, 6, 9, 12, 15, 18, 21, 24, 23, 22, 19, 16, 13, 10, 7, 4, 5, 8, 11, 14, 17, 20 ]
 
@@ -42,3 +42,44 @@ spiralTraversal([[ 1 ], [ 2 ], [ 3 ], [ 4 ]])	// [ 1, 2, 3, 4 ]
 
 spiralTraversal([[ 1, 2, 3, 4, 5, 6, 7 ]]) // [ 1, 2, 3, 4, 5, 6, 7 ]
 */
+
+
+function spiralTraversal(matrix) {
+  var rows = matrix.length
+  var cols = matrix[0].length
+
+  var complete = rows * cols
+
+  var i = 0;
+  var j = 0;
+
+  var count = 0;
+  var round = 0;
+
+  var result = []
+  var movement = [
+    { i: 0, j: 1 },
+    { i: 1, j: 0 },
+    { i: 0, j: -1 },
+    { i: -1, j:0 }
+  ]
+
+  while(count < complete) {
+    currentMovement = movement[round%4]
+    if (result.indexOf(matrix[i][j]) < 0) {
+      result.push(matrix[i][j])
+      count++;
+    }
+
+    i = i + currentMovement.i
+    j = j + currentMovement.j
+
+    if(!matrix[i] || !matrix[i][j] || result.indexOf(matrix[i][j]) >= 0 ) {
+      round++
+      i = i - currentMovement.i
+      j = j - currentMovement.j
+    }
+
+  }
+  return result;
+}
