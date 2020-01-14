e2dc823d062209aaa8475c16af0c6d4ad350ce41
henimezrani <henimezrani@gmail.com>
2020-01-14 09:53:38 +0100

    done

--- ArrayCeption/arrayCeption.js
+++ ArrayCeption/arrayCeption.js
@@ -15,3 +15,28 @@
 //  [ ] ==>	0
 //  array:
 //  [ [ [ ] ] ] ==>	0
+
+function arrayception(array) {
+  var stringifiedArray = JSON.stringify(array);
+  var max = 0;
+  var count = 0;
+  var boo = false;
+  for (var i = 0; i < stringifiedArray.length; i++) {
+    debugger;
+    if (stringifiedArray[i] === "[") {
+      count++;
+    } else if (stringifiedArray[i] === "]" && boo) {
+      if (count > max) {
+        max = count
+      }
+      boo = false;
+      count--
+    } else if (stringifiedArray[i] === "]") {
+      count--;
+    } else if (stringifiedArray[i] !== ","){
+      boo = true;
+    }
+  }
+return max
+}
+
