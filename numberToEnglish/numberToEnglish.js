c5347f086b2e7963729a34042bdb8336c17ba3ab
henimezrani <henimezrani@gmail.com>
2020-01-21 11:07:41 +0100

    not solved

--- numberToEnglish/numberToEnglish.js
+++ numberToEnglish/numberToEnglish.js
@@ -43,16 +43,69 @@ var numbersToWords = {
 };
 
 var numbersToPlace = {
-  10: 'ten',
-  100: 'hundred',
-  1000: 'thousand',
-  1000000: 'million',
-  1000000000: 'billion',
-  1000000000000: 'trillion',
-  1000000000000000: 'quadrillion',
-  1000000000000000000: 'quintillion'
+  10: 'ten',                          //2
+  100: 'hundred',                     //3
+  1000: 'thousand',                   //4
+  1000000: 'million',                 //7
+  1000000000: 'billion',              //9
+  1000000000000: 'trillion',          //12
+  1000000000000000: 'quadrillion',    //15
+  1000000000000000000: 'quintillion'  //18
 };
 
-function numberToEnglish(number) {
-  // your code here...
+function numberToEnglish(number, result) {
+  result = result || ''
+  console.log(result)
+  if (number == 0) return result
+  if (number.toString().length < 4) {
+    return result + ' ' + maxThreeDigitNumberToEnglish(number)
+  } else {
+    var base = number.toString().length
+    var start = base
+    while (base > 2 || number !== 0) {
+      while(!numbersToPlace[Math.pow(10,start)]) {
+        start--;
+      }
+      console.log('hi')
+      var tmpNum;
+      if (start === base ) {
+        start = start - 3
+        tmpNum = number.toString().slice(0, 3)
+      } else {
+        tmpNum = number.toString().slice(0, base - start)
+      }
+      result += maxThreeDigitNumberToEnglish(parseInt(number.toString().slice(0, base-start))) + " " + numbersToPlace[Math.pow(10,start)] + ' '
+      numberToEnglish((parseInt(number.toString().slice(base-start))), result)
+    }
+  }
+}
+
+
+
+  // while (base > 0) {
+  //   var base = number.toString().length
+  //   while(!numbersToPlace[length]) {
+  //     base--;
+  //   }
+  //   new tmpNbr = Math.floor(number / 10*(base-1))
+  // }
+  // main = Math.floor(number/base)
+  // rest = number%base
+
+
+function maxThreeDigitNumberToEnglish(number) {
+  switch(number.toString().length) {
+    case 1:
+      return numbersToWords[number]
+      break;
+    case 2:
+      if (numbersToWords[number]) {
+        return numbersToWords[number]
+      }
+      return numbersToWords[Math.floor(number/10)*10] + ' ' + numbersToWords[number%10]
+      break;
+    case 3:
+      return numbersToWords[Math.floor(number/100)] + ' hundred ' + maxThreeDigitNumberToEnglish(number%100)
+      break;
+  }
 }
