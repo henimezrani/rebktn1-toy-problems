988fdaa813217bdf445adc0a89b3533e1111a38e
henimezrani <henimezrani@gmail.com>
2020-02-15 09:30:00 +0100

    solved

--- implementOperators/implementOperators.js
+++ implementOperators/implementOperators.js
@@ -12,13 +12,44 @@ EXAMPLES:
 */
 
 var multiply = function(x, y) {
-  // your code here...
+  if (x < y) {
+    return multiply(y, x);
+  }
+  var result = 0;
+  for (var i = Math.abs(y); i > 0; i--) {
+    result += x;
+  }
+  if (y < 0) {
+    result = -result;
+  }
+
+  return result;
 };
 
 var divide = function(x, y) {
-  // your code here...
+  if (y === 0) {
+    return "error";
+  }
+
+  var quotient = 0;
+  var divisor = -Math.abs(y);
+
+  for (i = Math.abs(x); i >= Math.abs(divisor); i += divisor) {
+    console.log(i, divisor);
+    console.log(quotient);
+    quotient++;
+  }
+
+  if ((x < 0 && y > 0) || (x > 0 && y < 0)) {
+    quotient = -quotient;
+  }
+
+  return quotient;
 };
 
 var modulo = function(x, y) {
-  // your code here...
+  var result = x;
+  while (result >= y) result -= y;
+
+  return result;
 };
